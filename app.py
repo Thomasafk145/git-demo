effbac24c7f1ee79cd895f038a734b8b9dd4cbba
Thomas <thomascarp3nt3r@gmail.com>
2026-01-07 12:49:38 -0500

    added a print

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Git/Github Demo")
-print("hello from the local repo")
-print("hello from the remote repo")
+print("hello from the local repo 0")
+print("hello from the remote repo 1")
+print("Hey remote, you dumb")
