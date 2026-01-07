467ae6f6eb831b20d0e20db113d7a4948798b000
Thomasafk145 <thomascarp3nt3r@gmail.com>
2026-01-07 12:49:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Git/Github Demo")
-print("hello from the local repo")
-print("hello from the remote repo")
+print("hello from the local repo 2")
+print("hello from the remote repo 3")
+print("Hey local, you ain't so tuff")
